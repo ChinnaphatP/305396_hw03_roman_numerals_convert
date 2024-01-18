2b4ec3c70035c46c7c39854ecea0f314323c4057
Chinnaphat <chinnaphat46@gmail.com>
2024-01-18 09:54:02 +0700

    Design test and function

--- Numerals/roman.py
+++ Numerals/roman.py
@@ -1,5 +1,8 @@
 """The Roman numerals Module."""
 
+class NumberOutOfRange(ValueError):
+    """An extension in the standard exception."""
 
-def to_roman():
+
+def to_roman(arabic):
     """Convert Hindu-Arabic number to Roman number."""
--- tests/numeral/test_roman.py
+++ tests/numeral/test_roman.py
@@ -1,8 +1,45 @@
 """Test cases for Roman numerals."""
 
+# Standard library
+
+# Third party library
+import pytest
+
+# Project library
 from Numerals.roman import to_roman
 
 
-def test_calling_function():
-    """Call a roman numeral function."""
-    to_roman()
+@pytest.mark.parametrize(
+    "arabic_number, roman_number",
+    [
+        # Base case
+        (1, "I"),
+        (5, "V"),
+        (10, "X"),
+        (50, "L"),
+        (100, "C"),
+        (500, "D"),
+        (1000, "M"),
+        
+        # Addition case
+        (2, "II"),
+        (3, "III"),
+        (6, "VI"),
+        (8, "VII"),
+        
+        # Subtraction case
+        (4, "IV"),
+        (9, "IX"),
+        
+    ]
+)    
+
+def test_to_roman(arabic_number, roman_number):
+    """Convert Hindu-Arabic number to Roman number."""
+    # Arrange
+    
+    # Act
+    result = to_roman(arabic_number)
+    
+    # Assert
+    assert result == roman_number
